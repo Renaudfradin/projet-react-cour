be476014a8741d5d3b6de16878237b3aee5e5e28
Renaud <renaudfradin@gmail.com>
2020-10-28 10:23:15 +0100

    creations 1er composent

---
+++ src/components/Article/Article.js
@@ -0,0 +1,8 @@
+function Article(){
+    return(
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
+}
+export default Article;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Article from './components/Article/Article';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Article />
   </React.StrictMode>,
   document.getElementById('root')
 );

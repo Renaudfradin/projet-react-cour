fde7fbb9ecdbe2c66c8e5937ab6cb866206b0170
Renaud <renaudfradin@gmail.com>
2020-10-29 15:26:27 +0100

    filter article title, category and publiched

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,19 @@ import {useEffect,useState} from 'react';
 import useArticle from '../../hooks/useArticle/useArticle.js'
 import useCategory from '../../hooks/useCategory/useCategory.js'
 import List from '../List/List.js'
-import Listitem from '../Listitem/Listitem.js'
+//import Listitem from '../Listitem/Listitem.js'
 import Title from '../Title/Title.js'
 import Filter from '../Filter/Filter.js'
 function App() {
     const [titre, setTitre]=useState("projet cour");
-    const articles = useArticle();
-    const categories = useCategory();
+    const articles  = useArticle();
+    const categories  = useCategory();
 
     useEffect(()=>{
         setTitre(titre,document.title=titre);
     },[titre])
 
-
+/*
     const produit = [
         {   
             "id":1,
@@ -42,21 +42,44 @@ function App() {
             "name":"PC4"
         }
     ]
+*/
+    /*
     const [title, settitle]=useState('Article');
     function handlchange(event){
         console.log(event.target.value);
         settitle(event.target.value);
     }
-    
-    const filteredarticle = articles
-        .filter(art => art.title.includes(title));
+
+    const [categorys, setcategorys]=useState('');
+    function handlchangecat(event){
+        setcategorys(event.target.value);
+    }
+    */
+    const [filters, setFilters] = useState({
+        title: '',
+        category: '',
+        published: ''
+    });
+    function handleFilterChange(event) {
+        setFilters({
+          ...filters,
+          [event.target.name]: event.target.value
+        });
+    }
+
+    const filteredArticles = articles
+        .filter(art => art.title.includes(filters.title))
+        .filter(art => filters.category === '' || art.category === Number(filters.category))
+        .filter(art => filters.published === '' || (art.published === true && filters.published === 'published') || (art.published === false && filters.published === 'draft'));
+
     return (
         <div>
-            <Filter title={title} handlchange={handlchange}></Filter>
+            <Filter categories={categories} category={filters.category} published={filters.published} title={filters.title} handleFilterChange={handleFilterChange}>
+            </Filter>
             <Title titre={titre}></Title>
-            <List articles={filteredarticle} categories={categories}></List>
-            <Listitem produit={produit}></Listitem>
+            <List articles={filteredArticles} categories={categories}></List>
         </div>
     );
 }
 export default App;
+/*<Listitem produit={produit}></Listitem>*/ 

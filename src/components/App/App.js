06aaece0ebb146b802326ceb1b1b2c3a70489942
Renaud <renaudfradin@gmail.com>
2020-11-17 11:53:03 +0100

    maj timer and app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,20 @@
+import {lazy, Suspense} from 'react'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import Header from '../Header/Header.js'
+import createContext  from '../../contexts/categories.js'
+import useCategory from '../../hooks/useCategory/useCategory.js'
+/*
 import ArticlePage from '../ArticlePage/ArticlePage.js'
 import ArticlesPage from '../ArticlesPage/ArticlesPage.js'
-import Header from '../Header/Header.js'
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
-import  createContext  from '../../contexts/categories.js'
-import useCategory from '../../hooks/useCategory/useCategory.js'
 import TicTacToe from '../TicTacToe/TicTacToe.js'
 import Timer from '../Timer/Timer.js'
+*/
+const ArticlePage = lazy(() => import ('../ArticlePage/ArticlePage.js'))
+const ArticlesPage = lazy(() => import ('../ArticlesPage/ArticlesPage.js'))
+const TicTacToe = lazy(() => import ('../TicTacToe/TicTacToe.js'))
+const Timer = lazy(() => import ('../Timer/Timer.js'))
+
+
 function App() {
     const categories =useCategory();
 
@@ -13,6 +22,7 @@ function App() {
         <createContext.Provider value={categories}>
             <BrowserRouter>
                 <Header></Header>
+                <Suspense fallback={Header}>
                 <Switch>
                     <Route exact path="/" component={ArticlePage}></Route>
                     <Route exact path="/article" component={ArticlesPage}></Route>        
@@ -21,6 +31,7 @@ function App() {
                     <Route exact path="/TicTacToe" component={TicTacToe}></Route>  
                     <Route component={()=> '404'}></Route> 
                 </Switch>
+                </Suspense>
             </BrowserRouter>
         </createContext.Provider>
         
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,38 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import './Timer.css'
 function Timer(){
-    const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [play, setplay] = useState(false);
+  const [step,setsetp] =useState(0);
 
   function toggle() {
-    setIsActive(!isActive);
+    setplay(!play);
   }
 
   function reset() {
     setSeconds(0);
-    setIsActive(false);
+    setplay(false);
   }
 
   useEffect(() => {
     let interval = null;
-    if (isActive) {
+    if (play) {
       interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
+        setSeconds(seconds => seconds + step);
       }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, seconds]);
+  }, [play, seconds, step]);
 
+  function handlechangesetp(event) {
+    setsetp(Number(event.target.value))
+  }
   return (
     <div className="app">
+      <input value={step} onChange={handlechangesetp} type="number" min="1"></input>
       <div className="time">
         {seconds}s
       </div>
       <div className="row">
-        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
-          {isActive ? 'Pause' : 'Start'}
+        <button className={`button button-primary button-primary-${play ? 'active' : 'inactive'}`} onClick={toggle}>
+          {play ? 'Pause' : 'Start'}
         </button>
         <button className="button" onClick={reset}>
           Reset
